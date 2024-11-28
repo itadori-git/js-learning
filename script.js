6d4048c244fb6e40716bcea3f038bead74f976aa
Mishan Ghale Gurung <gurungmishan22@gmail.com>
2024-11-28 09:02:51 +0545

    pushing the js file

---
+++ script.js
@@ -0,0 +1,64 @@
+// console.log("Hello World");
+// alert("hello");
+
+
+// document.write("Hello world");
+
+// firstName=prompt("Enter your name");
+// console.log(firstName);
+// ----------------------Variable-------
+// x=10;
+// console.log(x);
+// _firstName="Ramu";
+// console.log(_firstName);
+
+//----------------DataTypes
+// string,Number,Boolean,Null,Undefined,Symbol,BigInt==Primitive data types
+// Object,Array,Function==Non-Primitive data types
+
+
+//string
+// fullName="my name is khan";
+// console.log( typeof fullName);
+
+
+
+//numbers
+//x=12, 12.5, 0,5
+// y=40
+// console.log( typeof y);
+
+
+// Boolean ==true && false
+//  isMarried=true;
+//  console.log( typeof isMarried);
+
+
+
+
+//BigInt
+// let num=9888888888888888n
+// console.log( typeof num);
+
+
+
+
+//null
+// x=null;
+// console.log( typeof x);
+
+
+
+
+
+// --------------undefined
+// let b=undefined;
+// console.log( typeof b);
+
+
+
+
+// let id =Symbol("Hello");
+// let id2 =Symbol("Hello");
+// console.log(id==id2);
+
