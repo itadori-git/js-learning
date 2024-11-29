d589858ff856e651ef362970b39fb93bc350a6e9
Mishan Ghale Gurung <gurungmishan22@gmail.com>
2024-11-29 09:12:59 +0545

    messaging about the things

--- script.js
+++ script.js
@@ -62,21 +62,117 @@
 // let id2 =Symbol("Hello");
 // console.log(id==id2);
 
-let a =("Mishan")
-let b = null;
-let c = 22;
-let d = true;
-let e = undefined;
-let f = 3.55;
- console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f);
+// let a =("Mishan")
+// let b = null;
+// let c = 22;
+// let d = true;
+// let e = undefined;
+// let f = 3.55;
+//  console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f);
 
 
- let p = {
-    "name" : "Mishan",
-    "job role" : "Developer",
-    "is_married" : false
- }
- console.log(p);
+//  let p = {
+//     "name" : "Mishan",
+//     "job role" : "Developer",
+//     "is_married" : false
+//  }
+//  console.log(p);
 
- p.salary=50000;
- console.log(p);
+//  p.salary=50000;
+//  console.log(p);
+
+// let x;
+// x=10;
+// console.log(x);
+
+// const a=9;
+// a=10;
+// console.log(a);
+
+// var s=10;
+// var s=20;
+// s=30;
+// console.log(s);
+
+// console.log("s")
+// var s=10;
+
+
+// let n1=1;
+// let n2=2;
+// let sum=n1%n2;
+
+// console.log(sum);
+
+
+// let a=10;
+// console.log(++a);
+
+//----------javascript  comparision operators--------------------//
+
+
+// let x=10;
+// let y=20;
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x==y);
+// console.log(x!=y);
+// console.log(x>=y);
+// console.log(x<=y);
+
+
+// let x=10;
+// let y="10";
+// console.log(x===y);
+
+// let x=20;
+// let y=20;
+// console.log(x>=y);
+
+
+// let x=false;
+// console.log(x?5:10);
+
+// const isMarried=true;
+// const result=isMarried? "Married":"Unmarried";
+// console.log(result);
+
+
+
+
+// let z=10;
+// z+=10; //z=z+10
+// console.log(z);
+
+
+// let z=cond1(true) && cond2(true);
+// let z(false) = cond1(false) && cond2(false);
+
+
+
+//-----------------------------conditional statement-----------------//
+
+// if(false){
+//    console.log("hello");
+// }
+// else{
+//    console.log("bye");
+// }
+
+
+// let x=12;
+// let y=20;
+// if(x>y){
+//    console.log("x is greater than y");
+// }
+// else{
+//    console.log("y is greater than x");
+// }
+
+
+
+
+
+
+
+
